94ae2c9805347b34ec15b35cdf70a159838e5554
Mickael Almeida <malmeida@et.esiea.fr>
2022-03-24 10:42:50 +0100

    Update and rename install to zshcustom.sh

---
+++ zshcustom.sh
@@ -0,0 +1,7 @@
+## Install oh-my-zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## Install zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
