39d7099286d366f5d70ef57d9c14e135d640ab50
Mickael Almeida <malmeida@et.esiea.fr>
2023-03-01 10:32:52 +0100

    Update zshcustom.sh

--- zshcustom.sh
+++ zshcustom.sh
@@ -1,7 +1,3 @@
-## Install oh-my-zsh
-
+mkdir -p ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-## Install zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
